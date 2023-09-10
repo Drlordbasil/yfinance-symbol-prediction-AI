4f4090a55dc84252b07c529dab884b34a64b0c45
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:45:57 -0400

    Auto-formatting code

--- script.py
+++ script.py
@@ -4,12 +4,14 @@ from tensorflow import keras
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
 
+
 def fetch_data(symbol, start, end):
     try:
         data = yf.download(symbol, start=start, end=end)['Close']
         return data
     except Exception as e:
         raise Exception(f"Error fetching data: {e}")
+
 
 def preprocess_data(data):
     try:
@@ -18,19 +20,23 @@ def preprocess_data(data):
     except Exception as e:
         raise Exception(f"Error preprocessing data: {e}")
 
+
 def convert_to_sequences(data_scaled, sequence_length):
     try:
-        X = [data_scaled[i:i + sequence_length] for i in range(len(data_scaled) - sequence_length)]
+        X = [data_scaled[i:i + sequence_length]
+             for i in range(len(data_scaled) - sequence_length)]
         y = data_scaled[sequence_length:]
         return np.array(X), np.array(y)
     except Exception as e:
         raise Exception(f"Error converting to sequences: {e}")
+
 
 def split_dataset(X, y, test_size):
     try:
         return train_test_split(X, y, test_size=test_size, shuffle=False)
     except Exception as e:
         raise Exception(f"Error splitting dataset: {e}")
+
 
 def build_model(sequence_length):
     try:
@@ -42,12 +48,15 @@ def build_model(sequence_length):
     except Exception as e:
         raise Exception(f"Error building model: {e}")
 
+
 def train_model(model, X_train, y_train, epochs=10, batch_size=32):
     try:
         model.compile(optimizer='adam', loss='mean_squared_error')
-        model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, verbose=0)
+        model.fit(X_train, y_train, epochs=epochs,
+                  batch_size=batch_size, verbose=0)
     except Exception as e:
         raise Exception(f"Error training model: {e}")
+
 
 def evaluate_model(model, X_val, y_val):
     try:
@@ -56,6 +65,7 @@ def evaluate_model(model, X_val, y_val):
         return mse
     except Exception as e:
         raise Exception(f"Error evaluating model: {e}")
+
 
 def make_realtime_prediction(model, scaler, sequence_length, symbol, start, end):
     try:
@@ -66,6 +76,7 @@ def make_realtime_prediction(model, scaler, sequence_length, symbol, start, end)
     except Exception as e:
         raise Exception(f"Error making real-time prediction: {e}")
 
+
 def main():
     try:
         symbols = ['AAPL', 'GOOGL', 'MSFT']
@@ -75,8 +86,10 @@ def main():
 
         data = [fetch_data(symbol, start_date, end_date) for symbol in symbols]
         data_scaled = [preprocess_data(d) for d in data]
-        X = np.concatenate([convert_to_sequences(d, sequence_length)[0] for d in data_scaled])
-        y = np.concatenate([convert_to_sequences(d, sequence_length)[1] for d in data_scaled])
+        X = np.concatenate([convert_to_sequences(
+            d, sequence_length)[0] for d in data_scaled])
+        y = np.concatenate([convert_to_sequences(
+            d, sequence_length)[1] for d in data_scaled])
         X_train, X_val, y_train, y_val = split_dataset(X, y, test_size=0.2)
 
         model = build_model(sequence_length)
@@ -94,6 +107,7 @@ def main():
 
     except Exception as e:
         print(f"Error: {e}")
+
 
 if __name__ == "__main__":
     main()
